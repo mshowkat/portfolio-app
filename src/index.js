395afbe8d61adf5e404b21f6ada9f053780ff068
mshowkat <bangleblaze@gmail.com>
2019-08-05 02:49:31 +0600

    base

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+//react-mdl 
+import 'react-mdl/extra/material.css';
+import 'react-mdl/extra/material.js';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
