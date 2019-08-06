2fd7a06b478cca86476c68ab6f3fc5f42ca1d32a
mshowkat <bangleblaze@gmail.com>
2019-08-06 17:10:38 +0600

    production

---
+++ build/precache-manifest.3f9b2725da9747f6cc7c7f999da79a7e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "547b8f8c9629b55cc19707ea08367366",
+    "url": "/portfolio-app/index.html"
+  },
+  {
+    "revision": "8103d378eee34a026078",
+    "url": "/portfolio-app/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "f17aba6dda517307a142",
+    "url": "/portfolio-app/static/css/main.0d71320b.chunk.css"
+  },
+  {
+    "revision": "8103d378eee34a026078",
+    "url": "/portfolio-app/static/js/2.3bf04afa.chunk.js"
+  },
+  {
+    "revision": "f17aba6dda517307a142",
+    "url": "/portfolio-app/static/js/main.19a5cad3.chunk.js"
+  },
+  {
+    "revision": "44340542c48e10c20cd6",
+    "url": "/portfolio-app/static/js/runtime~main.cb97d77a.js"
+  },
+  {
+    "revision": "fe67ee6cc7b9aae77c7330ee941d3ea6",
+    "url": "/portfolio-app/static/media/000.fe67ee6c.jpg"
+  },
+  {
+    "revision": "577362e68ce6e03054a63d0803393c9f",
+    "url": "/portfolio-app/static/media/01.577362e6.png"
+  },
+  {
+    "revision": "55d9657a17482772b3eb6cb580cd902c",
+    "url": "/portfolio-app/static/media/color.55d9657a.png"
+  },
+  {
+    "revision": "b1265dbefd192723fb1ba3c61247e475",
+    "url": "/portfolio-app/static/media/jsDoc.b1265dbe.png"
+  },
+  {
+    "revision": "362114efbb098872e4750233b5186d05",
+    "url": "/portfolio-app/static/media/rest.362114ef.png"
+  },
+  {
+    "revision": "b6df86e9f0b6bc1edac464016d9628d4",
+    "url": "/portfolio-app/static/media/todo.b6df86e9.png"
+  }
+]);
